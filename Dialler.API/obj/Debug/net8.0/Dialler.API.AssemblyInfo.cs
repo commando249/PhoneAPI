eec143bf680b9732ba8e81f0142bb61f9894b913
Craig <cdunlap3234@gmail.com>
2024-12-22 11:20:09 -0500

    DTO class fininshed

--- Dialler.API/obj/Debug/net8.0/Dialler.API.AssemblyInfo.cs
+++ Dialler.API/obj/Debug/net8.0/Dialler.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Dialler.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+72a876336bc6ec65fff5cb61830ba0dbe42e4305")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8a9aea50c241a7f3b88972e774c47c9336cb29e0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Dialler.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Dialler.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
